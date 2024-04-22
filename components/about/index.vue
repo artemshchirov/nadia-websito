5f0aa2b5acc824b4c02207b947d29f415d089f37
Artem Shchirov <artemshchirov@gmail.com>
2024-04-22 21:58:55 +0300

    feat(web): init /about page

--- components/about/index.vue
+++ components/about/index.vue
@@ -5,11 +5,57 @@
     <div class="about section">
       <SharedHeader />
 
-      <Lens />
+      <p class="about__greetings">Hello</p>
+      <p class="about__name">I'm Nadya Besson</p>
+
+      <NuxtImg
+        class="image image-about-1"
+        src="https://ik.imagekit.io/webbuilder/nadia-websito/about/telegram-cloud-photo-size-4-5992547463728316990-y%201_yjnw3_5cG.webp?updatedAt=1713214366965"
+        alt="Nadya Besson"
+      />
+
+      <p class="about__welcome">
+        Welcome to my world of photography, where each frame tells a story, and every moment is
+        filled with meaning. From the first touch of the camera, I realized that photography for me
+        is not just a hobby, but a way to express myself, to capture moments of happiness and beauty
+        around.
+      </p>
+
+      <NuxtImg
+        class="image image-about-2"
+        src="https://ik.imagekit.io/webbuilder/nadia-websito/about/telegram-cloud-photo-size-4-5992547463728316990-y%201_yjnw3_5cG.webp?updatedAt=1713214366965"
+        alt="Nadya Besson"
+      />
+
+      <NuxtImg
+        class="image image-about-3"
+        src="https://ik.imagekit.io/webbuilder/nadia-websito/about/telegram-cloud-photo-size-4-5992547463728316985-y%202_wrLzBmKpI.webp?updatedAt=1713214366925"
+        alt="Nadya Besson"
+      />
+
+      <p class="about__bio">
+        My passion for photography started in 2018, and since then, I have dedicated myself to the
+        art of capturing not only the external beauty but also the inner world of people. I strive
+        to create not just images, but stories that will inspire, delight, and evoke warm memories.
+      </p>
+
+      <NuxtImg
+        class="image image-about-4"
+        src="https://ik.imagekit.io/webbuilder/nadia-websito/about/IMG_5894%201_uAm-LRAEq5.webp?updatedAt=1713214365464"
+        alt="Nadya Besson"
+      />
+
+      <h2 class="about__education-title">My education&inspiration:</h2>
+      <h2 class="about__education-text">
+        My journey in photography began with studying at the Academy of Arts, where I not only
+        mastered the basics of design and composition but also learned to see beauty in simple
+        things. Then, training with renowned masters helped me develop my style and approach to
+        shooting.
+      </h2>
 
       <SharedCommonImages />
 
-      <Dividers />
+      <SharedDividers />
     </div>
 
     <Footer />
